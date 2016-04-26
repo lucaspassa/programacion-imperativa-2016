dbea73ed387939f8d5f8c9165dc3b228b3179517
Luks <lucaspassa@gmail.com>
2016-04-26 00:18:20 +0000

    Ejercicio dos arrays

---
+++ ejercicios/ejercicio2.c
@@ -0,0 +1,4 @@
+ /* Crear un programa llamado paresImpares que cree un array de 100 números aleatorios del 1 al 1000. 
+ Una vez creado, mostrar el contenido y después organizarlo de forma que estén juntos los elementos pares y los impares. 
+ Después, volver a mostrar el array
+ */
