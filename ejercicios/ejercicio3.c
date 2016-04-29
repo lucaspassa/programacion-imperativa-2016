0a32b9b1ced411d0cdd34e4c04a63d95b04c450d
Luks <lucaspassa@gmail.com>
2016-04-29 22:37:31 +0000

    ejercicios 3 y 4

---
+++ ejercicios/ejercicio3.c
@@ -0,0 +1,14 @@
+/********************************************************************************************************/ 
+/* Crear un programa llamado vendedores que cree un array de 18 X 10 indicando,
+/* que poseemos una empresa de 18 vendedores cada uno de los cuales vende 10 productos.
+/*
+/* El array almacena los ingresos obtenidos por cada vendedor en cada producto, 
+/* de modo que un menú permite almacenar los ingresos, revisar el total de cada vendedor, 
+/* y obtener los ingresos totales
+/**********************************************************************************************************/
+
+#include <stdio.h>
+
+void main() {
+    
+}
---
+++ ejercicios/ejercicio4.c
@@ -0,0 +1,12 @@
+/*******************************************************************************/
+/* Crear un programa que mediante un menú admita reservar o cancelar asientos de un avión, 
+/* así como mostrar qué asientos están ocupados y libres actualmente.
+/*
+/* El array tendrá 25 filas y 4 columnas
+/*******************************************************************************/
+
+#include <stdio.h>
+
+void main() {
+    
+}
